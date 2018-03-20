648f44ddc34e11d3d9a49fabb6cfadf80652b146
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-20 16:02:02 +0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux';
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,3 +20,30 @@ class App extends Component {
 }
 
 export default App;
+
+function reducer(state = { value: 1 }, action) {
+  // return { value: 10 };
+  if (action.type === 'INCREASE') return { value: state.value + 1 };
+  if (action.type === 'DESCREASE') return { value: state.value - 1 };
+  if (action.type === 'RESET') return { value: 1 };
+   return state;
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+store.dispatch({ type: 'INCREASE' });
+console.log(store.getState());
+store.dispatch({ type: 'INCREASE' });
+console.log(store.getState());
+store.dispatch({ type: 'DESCREASE' });
+console.log(store.getState());
+
+/*
+  - state {}
+  - store
+  - reducer -> function tinh toan state moi
+  - action { type: '' }
+
+
+*/
